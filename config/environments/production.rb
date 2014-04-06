38f65eb1287f6d6413d120b6dc5faf5be8843abb
wullaski <wullaski@gmail.com>
2014-04-06 11:34:46 -0400

    fixed syntax error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,5 @@ Portfolio::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  
-}
+
 end
